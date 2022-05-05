4a199f324b9d7d3ec847c6b22e33d3e49bd24d97
GuangGU47 <60858605+GuangGU47@users.noreply.github.com>
2022-05-05 23:29:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,5 +19,6 @@
 echo 'src-git mosdns https://github.com/QiuSimons/openwrt-mos' >>feeds.conf.default
 echo 'src-git aliyundrivewebdav https://github.com/messense/aliyundrive-webdav' >>feeds.conf.default
 echo 'src-git aliyundrivefuse https://github.com/messense/aliyundrive-fuse' >>feeds.conf.default
+echo 'src-git openwrtnicdrivers https://github.com/csrutil/OpenWrt-NIC-Drivers' >>feeds.conf.default
 #echo 'src-git ddns-scripts_aliyun https://github.com/mm2231031/ddns-scripts_aliyun' >>feeds.conf.default
 #echo 'src-git luci-app-socat https://github.com/kiddin9/openwrt-packages/tree/master/luci-app-socat' >>feeds.conf.default
